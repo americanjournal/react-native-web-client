18f328dc9ccd6b6dd555377a75bec6f973281f6d
jaredscheib <jared.scheib@gmail.com>
2016-11-19 20:58:59 -0800

    Fix style

--- src/scenes/Stories/Stories.js
+++ src/scenes/Stories/Stories.js
@@ -61,7 +61,7 @@ Stories.propTypes = {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
     backgroundColor: '#F5FCFF'
   },
